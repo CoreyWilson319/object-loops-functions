ba6055d1ed14182803ba4133c89b628fbade5831
Your Name <you@example.com>
2020-10-21 15:11:59 -0400

    [files] add star-wars.js , iphone.js and index.html files

---
+++ star-wars.js
@@ -0,0 +1,13 @@
+const description = {
+    name: "star wars",
+    channel: "disney",
+    title: "the mandalorian",
+    mainline: false,
+    storm_trooper: true,
+    characters: 3,
+    isMovie: false,
+    length: '1:00',
+    uploaded: '1M',
+    username: 'Star Wars',
+    title: 'Special Look | The Mandalorian | Disney+'
+}
